9aed46197e0d581ccd685dcc7606f7394fdf2900
Israel Douglas <israeldouglas25@gmail.com>
2022-08-26 18:11:08 -0300

    Ajustes nos cards de promoção e index, alem de inclusões de imagens e banner nas paginas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 200) {
+    if (window.scrollY >= 100) {
         nav.classList.add('active_nav');
     } else {
         nav.classList.remove('active_nav');
