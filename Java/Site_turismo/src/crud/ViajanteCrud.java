5c1064e1491c2ec4c3c53488db55fa7277da7af4
Israel Douglas <israeldouglas25@gmail.com>
2022-09-04 19:49:12 -0300

    Crianção das tabelas de dao e crud de viagem e permissoes

--- Java/Site_turismo/src/crud/ViajanteCrud.java
+++ Java/Site_turismo/src/crud/ViajanteCrud.java
@@ -9,14 +9,14 @@ public class ViajanteCrud {
 
 	public static void main(String[] args) {
 		ViajanteDAO viajanteDAO = new ViajanteDAO();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		int op = 0 ;
+
+		int op = 0;
 		int posicao = 0;
-		
+
 		int id = 0;
-	
+
 		String nome = "";
 		String email = "";
 		int idade = 0;
@@ -25,9 +25,8 @@ public class ViajanteCrud {
 		String cidade = "";
 		String estado = "";
 		String telefone = "";
-		
+
 		do {
-			
 			System.out.println("=== CRUD VIAJANTE ===");
 			System.out.println("1 - Cadastrar viajante");
 			System.out.println("2 - Consultar viajante");
@@ -37,8 +36,8 @@ public class ViajanteCrud {
 			System.out.println("0 - Sair");
 			op = sc.nextInt();
 			sc.nextLine();
-			
-			switch(op) {
+
+			switch (op) {
 			case 1:
 				System.out.println("Nome: ");
 				nome = sc.nextLine();
@@ -57,32 +56,25 @@ public class ViajanteCrud {
 				estado = sc.nextLine();
 				System.out.println("Telefone: ");
 				telefone = sc.nextLine();
-				
+
 				Viajante v1 = new Viajante(id, nome, email, idade, logradouro, bairro, cidade, estado, telefone);
-				
+
 				viajanteDAO.save(v1);
 				System.out.println("\n***  Cadastrou  ***\n");
-				break;			
+				break;
 			case 2:
 				for (Viajante v : viajanteDAO.getViajante()) {
-					System.out.println(
-							"ID: "+ v.getId()
-							+"\nNome: "+ v.getNome()
-							+"\nE-mail "+ v.getEmail()
-							+"\nIdade: "+ v.getIdade()
-							+"\nLogradouro: "+ v.getLogradouro()
-							+"\nBairro: "+ v.getBairro()
-							+"\nCidade "+ v.getCidade()
-							+"\nEstado "+ v.getEstado()
-							+"\nTelefone "+ v.getTelefone()
-							);				
-					System.out.println("\n");				
+					System.out.println("ID: " + v.getId() + "\nNome: " + v.getNome() + "\nE-mail " + v.getEmail()
+							+ "\nIdade: " + v.getIdade() + "\nLogradouro: " + v.getLogradouro() + "\nBairro: "
+							+ v.getBairro() + "\nCidade " + v.getCidade() + "\nEstado " + v.getEstado() + "\nTelefone "
+							+ v.getTelefone());
+					System.out.println("\n");
 				}
 				break;
 			case 3:
 				System.out.println("Digite o ID do viajante: ");
 				posicao = sc.nextInt();
-				sc.nextLine();	
+				sc.nextLine();
 				System.out.println("Atualize Nome: ");
 				nome = sc.nextLine();
 				System.out.println("Atualize E-mail: ");
@@ -100,39 +92,36 @@ public class ViajanteCrud {
 				estado = sc.nextLine();
 				System.out.println("Atualize Telefone: ");
 				telefone = sc.nextLine();
-				
+
 				Viajante v2 = new Viajante(posicao, nome, email, idade, logradouro, bairro, cidade, estado, telefone);
-				
+
 				viajanteDAO.update(v2);
 				System.out.println("Atualizado!");
 				break;
 			case 4:
 				System.out.println("Digite o ID do viajante que deseja EXCLUIR: ");
 				posicao = sc.nextInt();
-				
+
 				viajanteDAO.deleteById(posicao);
-				System.out.println("Viajante Excluido com sucasso.");
+				System.out.println("Viajante Excluido com sucesso.");
 				break;
 			case 5:
 				System.out.println("Digite o ID para buscar: ");
 				posicao = sc.nextInt();
-				
+
 				Viajante v3 = viajanteDAO.getViajanteById(posicao);
-				
-				System.out.println("ID: "+ v3.getId() +" Nome: "+ v3.getNome());
+
+				System.out.println("ID: " + v3.getId() + " Nome: " + v3.getNome());
 				break;
 			default:
-				System.out.println(op != 0 ? "Opcao invalida, digite novamente" : "Obrigado por viajar com a Paixao Nordestina");
-			
+				System.out.println(
+						op != 0 ? "Opcao invalida, digite novamente" : "Obrigado por viajar com a Paixao Nordestina");
+
 			}
-			
+
 		} while (op != 0);
-		
+
 		sc.close();
-		
-		
-		
-		
 
 	}
 
---
+++ Java/Site_turismo/src/dao/PermissaoDAO.java
@@ -0,0 +1,173 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import connection.ConnectionMySQL;
+import model.Permissoes;
+
+public class PermissaoDAO {
+
+	Connection conn = null;
+	PreparedStatement pstm = null;
+
+	public void save(Permissoes permissao) {
+		String sql = "INSERT INTO permissao(tipo) values( ?)";
+
+		try {
+
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+
+			pstm.setString(1, permissao.getTipo());
+			pstm.execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public List<Permissoes> getPermissoes() {
+		String sql = "SELECT * FROM permissao";
+
+		List<Permissoes> permissoes = new ArrayList<Permissoes>();
+		ResultSet rset = null;
+
+		try {
+
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+			rset = pstm.executeQuery();
+
+			while (rset.next()) {
+				Permissoes permissao = new Permissoes();
+
+				permissao.setId(rset.getInt("id_permissao"));
+				permissao.setTipo(rset.getString("tipo"));
+
+				permissoes.add(permissao);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return permissoes;
+	}
+
+	public void update(Permissoes permissao) {
+		String sql = "UPDATE permissao set tipo = ? WHERE id_permissao = ?";
+
+		try {
+
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+
+			pstm.setString(1, permissao.getTipo());
+			pstm.setInt(2, permissao.getId());
+			pstm.execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void deleteById(int id) {
+		String sql = "DELETE FROM permissao WHERE id_permissao = ?";
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+
+			pstm.setInt(1, id);
+
+			pstm.execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Permissoes getPermissaoById(int id) {
+		String sql = "SELECT * FROM permissao WHERE id_permissao = ?";
+		
+		Permissoes permissoes = new Permissoes();
+		
+		ResultSet rset = null;
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setInt(1, id);
+			
+			rset = pstm.executeQuery();
+			rset.next();
+			
+			permissoes.setId(rset.getInt("id_permissao"));
+			permissoes.setTipo(rset.getString("tipo"));
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return permissoes;
+	}
+}
