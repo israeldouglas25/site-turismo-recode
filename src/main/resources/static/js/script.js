317bbcdd7443af7b25d58644b8eb1ac10d4e3a76
Israel Douglas <israeldouglas25@gmail.com>
2022-10-23 16:18:05 -0300

    Conclusão da pagina de form de funcionario e ajustes finais na pagina layout

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,21 +7,3 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('active_nav');
     }
 });
-
-const card1Nome = document.querySelector('#card-1-nome');
-
-const mudarNome = () => {
-    card1Nome.textContent = 'teste';
-}
-
-const card2Nome = document.querySelector('#card-2-nome');
-const card3Nome = document.querySelector('#card-3-nome');
-const card4Nome = document.querySelector('#card-4-nome');
-const card5Nome = document.querySelector('#card-5-nome');
-const card6Nome = document.querySelector('#card-6-nome');
-const card7Nome = document.querySelector('#card-7-nome');
-const card8Nome = document.querySelector('#card-8-nome');
-const card9Nome = document.querySelector('#card-9-nome');
-
-
-//card1Nome.textContent = 'israel';
